565bbeab996411027a58554ac4a59d3161546383
Yohan <yohanospina64@gmail.com>
2025-01-23 14:05:41 -0500

    Cree la funcion mostrarListaAmigos() que busca en la lista y actualiza la lista llamada listaNombres, selecciona la ubicacion donde se mostrara la lista en el elemento html, para luego limpiar la lista y asi evitar duplicados, luego creamos un bucle for que recorre la listaNombres creando un nuevo elemento li en la lista del archivo html ul, luego le asigna el nombre en la listaNombre a el elemento li nuevo y lo agrega, por ultimo usa la funcion limparCaja() para quitar cualquir texto escrito y dejar limpio el espacio donde escribimos los nombres

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let listaNombres = [];
 let nombreAmigo;
 
+// Función para recorrer el array y actualizar la lista.
+function mostrarListaAmigos() {
+    elementoHTML = document.getElementById("#listaAmigos") // Seleccionamos la lista en el HTML
+    elementoHTML.innerHTML = ""; // Limpiamos la lista para evitar duplicados
+    
+    // Recorremos el array listaNombres con un bucle for
+    for (let i = 0; i < listaNombres.length; i++) {
+        let listaLi = document.createElement("li"); // Creamos un nuevo <li>
+        listaLi.textContent = listaNombres[i]; // Le asignamos el nombre del amigo
+        listaLi.appendChild(li); // Agregamos el <li> a la lista  
+    } 
+    limpiarCaja();
+}
+
 function limpiarCaja() {
     document.querySelector("#amigo").value = "";
 };
