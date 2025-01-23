2ccfb9694671d2e4fa01130121cd2402e503191a
Yohan <yohanospina64@gmail.com>
2025-01-23 14:21:05 -0500

    Arregle un error en la funcion agregarAmigo() donde seleccionaba el input directamente sin acceder al valor almacenado en este asi nombreAmigo = document.getElementById(amigo) le agrege .value al final para seleccionar el valor almacenado y quedo asi nombreAmigo = document.getElementById(amigo).value;

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function asignarTextoElemento(elemento, texto) {
 }
 
 function agregarAmigo() {
-    nombreAmigo = document.getElementById("amigo")
+    nombreAmigo = document.getElementById("amigo").value;
 
     if (nombreAmigo == "") {
         alert("Por favor, inserte un nombre.")
