76844e8f91382528f58c1a46967a369767bcd18e
Yohan <yohanospina64@gmail.com>
2025-01-23 12:58:50 -0500

    Agregue funcion asignarTextoElemento que asigna un texto a los elementos html

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let listaNombres = [];
 let nombreAmigo;
 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
 function agregarAmigo() {
     nombreAmigo = document.getElementById("amigo")
 
