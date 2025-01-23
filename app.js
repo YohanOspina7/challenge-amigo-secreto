6acc0b7a695c549cff4bd52daf45809ba6c2741e
Yohan <yohanospina64@gmail.com>
2025-01-22 20:59:55 -0500

    Añadiendo funcion agregarAmigo() que almacena en la variable nombreAmigo el texto ingresado en el input con id amigo, luego valida si la variable esta vacia y manda una alerta, de lo contrario almacena el texto de la varible amigo dentro de la lista llamada listaNombres

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let listaNombres = [];
+let amigo;
+
+function agregarAmigo() {
+    amigo = document.getElementById("amigo")
+
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre.")
+        return;
+    } else {
+        listaNombres.push(amigo)
+    }
+};
